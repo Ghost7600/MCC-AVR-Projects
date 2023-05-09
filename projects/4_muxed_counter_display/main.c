d74614c7534ff40c3257bbb60f35dff5a17b6a55
Kenner Marqueti Couto <kenner.trabalhos@gmail.com>
2023-05-09 09:27:21 -0300

    started implementing task 4 - 7seg multiplexed display

---
+++ projects/4_muxed_counter_display/main.c
@@ -0,0 +1,24 @@
+
+#include <avr/io.h>
+
+int main()
+{
+    uint8_t hour=12;
+    uint8_t minutes=34;
+    uint8_t seconds=56;
+
+    vbool_t changetTime = false;
+
+    //timer1 configuration
+    // timer1.init();
+    // timer1.setCompareAValue();
+    // timer1.activateCompareAInterrupt();
+
+    while(1) {
+        if (changetime){
+            (seconds=59)?0;
+        }
+    }
+
+    return 0;
+}
