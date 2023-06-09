33069d24cd67073d4424a5e3b1e7f536198fb0bb
kenner.trabalhos@gmail.com <74597805+Ghost7600@users.noreply.github.com>
2023-06-09 02:26:59 -0300

    solved problem with pwm servo, works now, got to implement keypad states and logic

---
+++ projects/7_pwmservo/build/main.d
@@ -0,0 +1,16 @@
+build/main.o: main.cpp funsape/globalDefines.hpp funsape/pinout.hpp \
+ funsape/peripheral/timer1.hpp funsape/peripheral/../globalDefines.hpp \
+ funsape/peripheral/../util/debug.hpp \
+ funsape/peripheral/../util/../globalDefines.hpp \
+ funsape/device/hd44780.hpp funsape/device/../globalDefines.hpp \
+ funsape/device/../util/debug.hpp funsape/device/keypad.hpp
+funsape/globalDefines.hpp:
+funsape/pinout.hpp:
+funsape/peripheral/timer1.hpp:
+funsape/peripheral/../globalDefines.hpp:
+funsape/peripheral/../util/debug.hpp:
+funsape/peripheral/../util/../globalDefines.hpp:
+funsape/device/hd44780.hpp:
+funsape/device/../globalDefines.hpp:
+funsape/device/../util/debug.hpp:
+funsape/device/keypad.hpp:
--- projects/7_pwmservo/main.cpp
+++ projects/7_pwmservo/main.cpp
@@ -15,7 +15,18 @@
 int main()
 {
     Hd44780 lcd;
+
+    //Config Keypad
     Keypad keypad;
+    keypad.setPorts(&DDRD,PD0,&DDRD,PD4);
+    keypad.setKeyValues(Keypad::Type::KEYPAD_4X4,
+            '7', '8', '9', 'A',
+            '4', '5', '6', 'B',
+            '1', '2', '3', 'C',
+            'E', '0', 'F', 'D');
+    keypad.init(10);
+
+
 
 
     //Configure LCD
@@ -24,31 +35,41 @@ int main()
     lcd.init(Hd44780::Size::LCD_16X2, Hd44780::Font::FONT_5X8, true, true);
     lcd.stdio();
 
-    printf("   KEYPAD ADC   \n\n");
+    printf("   Good Morning! :-)   \n\n");
     delayMs(500);
     lcd.clearScreen();
-
+    setBit(DDRB,PB1);
+    setBit(DDRB,PB2);
     Timer1 timer1;
     timer1.setOutputMode(Timer1::OutputMode::NON_INVERTING_MODE,
             Timer1::OutputMode::NORMAL);   // Fast-PWM, non inverting mode, normal
-    timer1.init(Timer1::Mode::PWM_FAST_ICR, Timer1::ClockSource::PRESCALER_8);                  // prescaler = 8
-    timer1.setInputCaptureValue(39999);                                                         // Value ICR=39999
-    timer1.setCompareAValue(1999);           // Value OCR1A=1999
-    lcd.clearScreen();
-    printf("finished timer cofiguration");
-    delayMs(500);
-    while(1) {
-        lcd.clearScreen();
-        printf("entered while loop");
-        OCR1A = 1999; // Set position to 0 degrees
-        _delay_ms(500);
-        lcd.clearScreen();
-        OCR1A = 2999; // Set position to 90 degrees
-        _delay_ms(500);
-        OCR1A = 3999; // Set position to 180 degrees
-        _delay_ms(500);
-        OCR1A = 2999; // Set position to 90 degrees
+    timer1.init(Timer1::Mode::PWM_FAST_ICR, Timer1::ClockSource::PRESCALER_8);
+    timer1.setInputCaptureValue(0xFFFF); //Define ICR, portanto período da onda.
+    timer1.setCompareAValue(42000);           // Define a largura do pulso, Largura do Pulso(ms) = OCRA/2000, 21ms
+    timer1.setCompareAValue(20000);           // Define a largura do pulso, Largura do Pulso(ms) = OCRA/2000, 21ms
+    //posição do servo f(x)=({x-2000}/40000-0.5)*270 p/ range de -135 a +135  ou ainda pos = ({x-bottom}/(top-bottom)-0.5)*range
+    //cada grau ~= 223 contas p 180 graus
+    //para 270 graus cada 1 grau = 148 contas
+    //para um angulo ang, o valor de OCRA1 deve ser ({ang+135}/135)/2*40000+2000
+    int aux = 1999;
+    timer1.setCompareAValue(aux);
 
+
+
+
+
+
+
+
+
+
+    while(1) {
+        OCR1A = 35333; // Set position to minimum degrees, 1ms pulse width
+        // _delay_ms(500);
+        // aux = aux + 148;
+        // if (aux > 42000){
+        //     aux = 1999;
+        // }
     }
 
     return 0;
